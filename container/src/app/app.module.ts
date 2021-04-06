3dd701ad3b823f2aca431652d495c0576ef76d16
Leonardo Barberis <lbarberis@simtlix.com>
2021-04-06 11:41:22 -0300

    Added Shell

--- container/src/app/app.module.ts
+++ container/src/app/app.module.ts
@@ -1,15 +1,23 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WidgetComponent } from './widget/widget.component';
 import { LazyElementsModule } from '@angular-extensions/elements';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [AppComponent, WidgetComponent],
   imports: [BrowserModule, LazyElementsModule, NoopAnimationsModule],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {}
+  ngDoBootstrap() {
+    const el = createCustomElement(AppComponent, {
+      injector: this.injector,
+    });
+    customElements.define('app-root', el);
+  }
+}
