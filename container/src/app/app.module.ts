a9b33d53ed6ab80f4de1bc04767c64f869dc0bc5
Leonardo Barberis <lbarberis@simtlix.com>
2021-04-06 15:18:41 -0300

    Added Header in Container

--- container/src/app/app.module.ts
+++ container/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { WidgetComponent } from './widget/widget.component';
 import { LazyElementsModule } from '@angular-extensions/elements';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { createCustomElement } from '@angular/elements';
+import { HeaderComponent } from './header/header.component';
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, WidgetComponent],
-  imports: [BrowserModule, LazyElementsModule, NoopAnimationsModule],
+  declarations: [AppComponent, WidgetComponent, HeaderComponent],
+  imports: [BrowserModule, LazyElementsModule, NoopAnimationsModule, MatIconModule, MatToolbarModule, MatButtonModule],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
